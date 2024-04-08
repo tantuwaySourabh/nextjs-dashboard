212b9e88fea453eba05715638a5d305bd8fb564c
tantuwaySourabh <sourabhtan102@gmail.com>
2024-04-08 00:16:43 -0500

    Completed till navigation in nextjs

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Inter({ 
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
